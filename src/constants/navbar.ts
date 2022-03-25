9394773e54f76d6c1e1e8cd3bcaffa3576841a12
nadyacherepaha <nad.cherepaha@gmail.c>
2022-03-25 13:51:34 +0300

    Create routes for testing

---
+++ src/constants/navbar.ts
@@ -0,0 +1,40 @@
+export const routesForAuthUser = [
+  {
+    path: '/#/',
+    title: 'Home',
+    icon: '',
+  },
+  {
+    path: '/#/editor',
+    title: 'New Article',
+    icon: '',
+  },
+  {
+    path: '/#/settings',
+    title: 'Settings',
+    icon: '',
+  },
+  {
+    path: '/#/profile/:username',
+    component: 'Username',
+    exact: false,
+  },
+];
+
+export const routesForGuest = [
+  {
+    path: '/#/',
+    title: 'Home',
+    icon: '',
+  },
+  {
+    path: '/#/login',
+    title: 'Sign In',
+    icon: '',
+  },
+  {
+    path: '/#/register',
+    title: 'Sign Up',
+    icon: '',
+  },
+];
