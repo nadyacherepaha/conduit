635ce5cab8a0f06011b828235fbd324f8f1979c2
nadyacherepaha <nad.cherepaha@gmail.c>
2022-03-25 13:49:18 +0300

    Create tests for header

---
+++ src/tests/header.test.tsx
@@ -0,0 +1,25 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Header from '../components/header/Header';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('Header component', () => {
+  beforeEach(() => {
+    render(<Header />, { wrapper: MemoryRouter });
+  });
+  it('should have a logo', () => {
+    const logo = screen.getByTestId('header-logo');
+
+    expect(logo).toBeInTheDocument();
+  });
+  it('should have a navbar', () => {
+    const navbar = screen.getByTestId('nav-menu');
+
+    expect(navbar).toBeInTheDocument();
+  });
+});
