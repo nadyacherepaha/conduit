143e26187312f1ecbd0a6176ca4b58a17620e529
nadyacherepaha <nad.cherepaha@gmail.com>
2022-03-27 16:57:55 +0300

    Move the test folder to the top level

--- tests/header.test.tsx
+++ tests/header.test.tsx
@@ -5,7 +5,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import Header from '../components/header/Header';
+import Header from '../src/components/header/Header';
 import { MemoryRouter } from 'react-router-dom';
 
 describe('Header component', () => {
