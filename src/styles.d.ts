cb61e111d7e9d2930729d1cb3ecebf65d4161fdb
nadyacherepaha <nad.cherepaha@gmail.c>
2022-03-29 16:00:44 +0300

    Declare module for styles

---
+++ src/styles.d.ts
@@ -0,0 +1,24 @@
+declare module "*.scss" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.css" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
