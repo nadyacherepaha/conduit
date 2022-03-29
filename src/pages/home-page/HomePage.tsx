57d87bc2b046df4b2d481358e21131b8c3d3cb43
nadyacherepaha <nad.cherepaha@gmail.c>
2022-03-29 16:05:43 +0300

    Add a test home page

---
+++ src/pages/home-page/HomePage.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import style from '../../styles/main.module.scss';
+
+const HomePage: FC = () => {
+  return (
+    <div className={style.container}>
+      <h1>Test page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
