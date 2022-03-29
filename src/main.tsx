a0a119615dc63d4c7dc5dcd5c269f32711565dd9
nadyacherepaha <nad.cherepaha@gmail.c>
2022-03-29 16:06:54 +0300

    Add page routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,29 @@ import React, { FC, StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/header/Header';
+import HomePage from './pages/home-page/HomePage';
+import {
+  homePath,
+  settingsPath,
+  loginPath,
+  registerPath,
+  editorPath,
+  profilePath,
+} from './constants/navbar';
+import './styles/main.module.scss';
 
 const App: FC = () => {
   return (
     <StrictMode>
       <Router>
+        <Header />
         <Routes>
-          <Route path="/" element={<Header />} />
+          <Route path={homePath} element={<HomePage />} />
+          <Route path={loginPath} element={<HomePage />} />
+          <Route path={registerPath} element={<HomePage />} />
+          <Route path={editorPath} element={<HomePage />} />
+          <Route path={profilePath} element={<HomePage />} />
+          <Route path={settingsPath} element={<HomePage />} />
         </Routes>
       </Router>
     </StrictMode>
