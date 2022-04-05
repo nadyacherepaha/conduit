1373ad737f92e2718ffcb7becc08d69af825759b
nadyacherepaha <nad.cherepaha@gmail.com>
2022-04-05 11:59:15 +0300

    Move static text to a constant

--- src/components/tags/PopularTags.tsx
+++ src/components/tags/PopularTags.tsx
@@ -1,24 +1,28 @@
 import React, { FC } from 'react';
-import { Link } from 'react-router-dom';
 import classNames from 'classnames';
 import style from './tags.module.scss';
+
+const tagList: string[] = [
+  'welcome',
+  'implementations',
+  'codebaseShow',
+  'introduction',
+];
+
+const onFilterTags = () => {};
 
 const PopularTags: FC = () => {
   return (
     <div className={classNames(style.container, style.tags)}>
       <h3 className={style.title}>Popular Tags</h3>
-      <Link className={classNames(style.tagItem, style.bgColor)} to="/">
-        welcome
-      </Link>
-      <Link className={classNames(style.tagItem, style.bgColor)} to="/">
-        implementations
-      </Link>
-      <Link className={classNames(style.tagItem, style.bgColor)} to="/">
-        codebaseShow
-      </Link>
-      <Link className={classNames(style.tagItem, style.bgColor)} to="/">
-        introduction
-      </Link>
+      {tagList.map((title) => (
+        <button
+          key={title}
+          className={classNames(style.tagItem, style.bgColor)}
+          onClick={onFilterTags}>
+          {title}
+        </button>
+      ))}
     </div>
   );
 };
