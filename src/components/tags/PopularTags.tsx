93dd4df9b115903a62b14413fa7577f5a03afac7
nadyacherepaha <nad.cherepaha@gmail.com>
2022-04-06 12:01:12 +0300

    Add a request to get popular tags

--- src/components/tags/PopularTags.tsx
+++ src/components/tags/PopularTags.tsx
@@ -1,26 +1,36 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import classNames from 'classnames';
 import style from './tags.module.scss';
-
-const tagList: string[] = [
-  'welcome',
-  'implementations',
-  'codebaseShow',
-  'introduction',
-];
+import BASE_URL from '../../utils/baseUrl';
 
 const onFilterTags = () => {};
 
 const PopularTags: FC = () => {
+  const [tags, setTags] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${BASE_URL}tags`);
+        const result = await response.json();
+        setTags(result.tags);
+      } catch (e) {
+        const errorMessage = 'Something went wrong';
+        console.error(errorMessage);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className={classNames(style.container, style.tags)}>
       <h3 className={style.title}>Popular Tags</h3>
-      {tagList.map((title) => (
+      {tags.map((tag, key) => (
         <button
-          key={title}
+          key={key}
           className={classNames(style.tagItem, style.bgColor)}
           onClick={onFilterTags}>
-          {title}
+          {tag}
         </button>
       ))}
     </div>
