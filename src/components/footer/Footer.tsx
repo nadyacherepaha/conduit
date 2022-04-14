a09331d7030d9cbf003266bc17ba383afe05ff12
nadyacherepaha <nad.cherepaha@gmail.com>
2022-04-14 16:56:52 +0300

    Add footer component

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithubAlt } from '@fortawesome/free-brands-svg-icons';
+import style from './footer.module.scss';
+
+const Footer = () => {
+  return (
+    <footer className={style.footer}>
+      <Link className={style.link} to="/">
+        <FontAwesomeIcon icon={faGithubAlt} />
+        Fork on GitHub
+      </Link>
+    </footer>
+  );
+};
+
+export default Footer;
