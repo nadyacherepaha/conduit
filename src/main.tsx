490e8bfa4e148474bde56fe6eb4968d3a2d2f28d
nadyacherepaha <nad.cherepaha@gmail.com>
2022-06-23 14:48:02 +0200

    Add a provider to a parent component

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React, { FC, StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import setupStore from './redux/store/store';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import HomePage from './pages/home-page/HomePage';
@@ -13,22 +14,27 @@ import {
   profilePath,
 } from './constants/navbar';
 import './styles/main.module.scss';
+import { Provider } from 'react-redux';
 
 const App: FC = () => {
+  const store = setupStore();
+
   return (
     <StrictMode>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path={homePath} element={<HomePage />} />
-          <Route path={loginPath} element={<HomePage />} />
-          <Route path={registerPath} element={<HomePage />} />
-          <Route path={editorPath} element={<HomePage />} />
-          <Route path={profilePath} element={<HomePage />} />
-          <Route path={settingsPath} element={<HomePage />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path={homePath} element={<HomePage />} />
+            <Route path={loginPath} element={<HomePage />} />
+            <Route path={registerPath} element={<HomePage />} />
+            <Route path={editorPath} element={<HomePage />} />
+            <Route path={profilePath} element={<HomePage />} />
+            <Route path={settingsPath} element={<HomePage />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </Provider>
     </StrictMode>
   );
 };
