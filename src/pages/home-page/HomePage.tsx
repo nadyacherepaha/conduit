5f808e24781d121d252a8e80b99c5743e4838430
nadyacherepaha <nad.cherepaha@gmail.com>
2022-06-23 15:01:49 +0200

    Add filter tabs to the Home page

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -3,12 +3,14 @@ import Banner from '../../components/banner/Banner';
 import style from '../../styles/main.module.scss';
 import FeedList from '../../components/feeds/FeedList';
 import PopularTags from '../../components/tags/PopularTags';
+import FilterTabs from '../../components/filter-tabs/FilterTabs';
 
 const HomePage: FC = () => {
   return (
     <>
       <Banner />
       <main className={style.feedContainer}>
+        <FilterTabs />
         <FeedList />
         <PopularTags />
       </main>
