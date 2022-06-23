8e048ea2b18299aea83c31e5c8c50d7e20e5ac60
nadyacherepaha <nad.cherepaha@gmail.com>
2022-06-23 14:46:34 +0200

    Add filtering tags to store

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,8 +1,10 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import userReducer from '../reducers/authReducer';
+import filterTagsReducer from '../reducers/filterTagsReducer';
 
 const reducers = {
   user: userReducer,
+  filterTags: filterTagsReducer,
 };
 
 const rootReducer = combineReducers({
