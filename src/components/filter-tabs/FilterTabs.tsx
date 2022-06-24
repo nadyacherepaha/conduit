33efaa8ccb1dc278766f21172e6af05c4cabc892
nadyacherepaha <nad.cherepaha@gmail.com>
2022-06-24 12:01:31 +0200

    Change the export/import of individual actions

--- src/components/filter-tabs/FilterTabs.tsx
+++ src/components/filter-tabs/FilterTabs.tsx
@@ -3,12 +3,11 @@ import classNames from 'classnames';
 import style from './filterTabs.module.scss';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import getFilteredTags from '../../redux/selectors/filterTagsSelector';
-import { filterTagsSlice } from '../../redux/reducers/filterTagsReducer';
+import { deleteSelectedTag } from '../../redux/reducers/filterTagsReducer';
 
 const FilterTabs = () => {
   const dispatch = useAppDispatch();
   const { tag } = useAppSelector(getFilteredTags);
-  const { deleteSelectedTag } = filterTagsSlice.actions;
 
   const onResetFilter = () => {
     dispatch(deleteSelectedTag());
