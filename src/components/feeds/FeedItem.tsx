c02eb3eb2376cac0f77c867b2994ed32d49a4a03
nadyacherepaha <nad.cherepaha@gmail.com>
2022-07-01 10:36:22 +0200

    Change the path to icons

--- src/components/feeds/FeedItem.tsx
+++ src/components/feeds/FeedItem.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-solid-svg-icons/faHeart';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames';
 import AvatarGroup from '../avatar-group/AvatarGroup';
